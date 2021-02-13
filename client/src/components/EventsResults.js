bcc87a9d63710ae24e12e2f136ae037c3d504b51
Timothy Balascak-Huber <70402692+tlbhuber@users.noreply.github.com>
2021-02-13 14:04:07 -0500

    pulled info from API and populated into site

--- client/src/components/EventsResults.js
+++ client/src/components/EventsResults.js
@@ -1,36 +1,48 @@
 import React, { Component } from 'react'
-// import EventsResultsCard from "./EventsResultsCard"
+import EventsResultsCard from "./EventsResultsCard"
 
 
 //eslint-disable-next-line
 class EventsResults extends Component{
     //eslint-disable-next-line
 
-        // e.results.performers
-        // e.results.performers.id
-        // e.results.performers.name
-        // e.results.performers.taxonomies.name
-            // This is to try to breakdown by categories
-        // e.results.performers.type
-        // e.results.performers.url
-
-
+        // e.results.events
+        // e.results.events.id
+            // id & key
+        // e.results.events.title
+            // name of performer/event
+        // e.results.events.performers.image
+            // picture
+        // e.results.events.performers.type
+            // category of event
+        // e.results.events.datetime_local
+            // date         
+        // e.results.events.url
+            // tickets/sales
+        // e.results.events.venue.name
+        // e.results.events.venue.address
+        // e.results.events.venue.extended_address  
+        
         render(){
-       console.log(this.props.results.performers)
-
-
+                console.log(this.props.results.events)
         return(
-            // this.props.results.events.map((e) => (
-            //     <EventsResultsCard
-        
-
-                    <div>
-
-                    </div>
-
-            
-            )
-        
+            this.props.results.events.map((e) => (
+                <EventsResultsCard
+                _id={e.id}
+                key={e.id}
+                title={e.title}
+                type={e.type}
+                venue={e.venue.name}
+                address={e.venue.address}
+                extended_address={e.venue.extended_address}
+                date={e.datetime_local}
+                image={e.performers[0].image}
+                url={e.url}
+                
+                >Hello</EventsResultsCard>     
+            ))
+      
+        )
     }
 }
 
