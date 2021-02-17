3bda30c371edfe270d793e4a0d4692ee9b9d3280
Timothy Balascak-Huber <70402692+tlbhuber@users.noreply.github.com>
2021-02-16 19:01:12 -0500

    update EventsResultsCard

--- client/src/components/EventsResultsCard.js
+++ client/src/components/EventsResultsCard.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 import { Card, Button, Row, Col } from 'react-bootstrap'
 import { IoSaveSharp, IoLocationSharp, IoTicketSharp, IoTimeSharp } from "react-icons/io5";
 import { BsImage } from "react-icons/bs";
-import { VscSymbolEvent } from "react-icons/vsc";
+import { BsFillLightningFill } from "react-icons/bs";
+import moment from 'moment';
+
 import "./EventsResultsCard";
 
 
@@ -14,9 +16,13 @@ class EventsResultsCard extends Component {
             <Card key={this.props.key}>
                 <Card.Header>
                     <Row>
+                    <h5>{this.props.title}</h5>
+                    </Row>    
+
+                    <Row>    
                         <Col>
-                            <h5>{this.props.title}</h5>
-                            <img src={this.props.image} alt={this.props.title} class="img-rest"/>
+                            
+                            <img src={this.props.image} alt={this.props.title} class="img-rest" align="center"/>
                         </Col>
                         <Col>
                             <Button ><IoSaveSharp /></Button>
@@ -26,13 +32,13 @@ class EventsResultsCard extends Component {
                 </Card.Header>
                 <Row>
                     <Col>
-                    <p><VscSymbolEvent/>: {this.props.type}</p>
-                    <p><BsImage/>: {this.props.venue}</p>
-                    <p><IoLocationSharp/>: {this.props.address}, {this.props.extended_address}</p>
+                    <p><BsFillLightningFill/> Event Type: {this.props.type}</p>
+                    <p><BsImage/> Venue Name: {this.props.venue}</p>
+                    <p><IoLocationSharp/> Venue Address: {this.props.address}, {this.props.extended_address}</p>
                     </Col>
                     <Col>
                     <p><IoTimeSharp/>: {this.props.date}</p>
-                    <a href={this.props.url} className="event-links" target="_blank"><IoTicketSharp/>Tickets</a>
+                    <a href={this.props.url} className="event-links" target="_blank"><IoTicketSharp/>Tickets/More Info</a>
                     </Col>
                 </Row>
             </Card>
