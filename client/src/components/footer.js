3b0e6b46b26ab38936c2256126135109d70481b1
hrajpal <hrajpal@ebrevia.com>
2021-02-27 13:25:15 -0500

    footer file

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -2,10 +2,21 @@ import React from "react";
 import "./footer.css"
 function Footer() {
   return (
-    <div className="footer">
-      <h5>This is footer</h5>
-      
-    </div>
+    <div class="d-flex flex-column">
+    <footer class="footer">
+      <div>
+        <a href="https://coreui.io">Axolotl</a>
+        <span>&copy; 2021</span>
+      </div>
+      <div class="ml-auto">
+        <span>Powered by</span>
+        <a href="https://coreui.io">Lindsey Baginski,Corrine Pusillo,Heena Rajpal,Britney Borrero,
+        Timothy Balascak-Huber
+
+        </a>
+      </div>
+    </footer>
+  </div>
   );
 }
 
