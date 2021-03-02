34da934049b8dcd2a997110639d2eafe285c7b00
hrajpal <hrajpal@ebrevia.com>
2021-03-02 18:52:13 -0500

    splash update

--- client/src/pages/Splash.js
+++ client/src/pages/Splash.js
@@ -8,13 +8,11 @@ export default function Splash() {
       <h1>Axolotl</h1>
       <h5>Never have a bad food & never feel alone! </h5>
       <p class="lead">
-        
          Our app would help in looking-up for the events and restaurants near you and also saving them for future 
            access. 
-           
-   
-        
+      
       </p>
+
       <Link
         to="/signup"
       >
